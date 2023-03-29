02f2a421160ed2615dc68757da178b71f03251d9
FrodeBerg <bergstedt.frode@gmail.com>
2023-03-29 18:22:02 +0200

    Add basic layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { useState } from "react";
+
 
 function App() {
   return (
     <div>
-      
+      <div className='scores'>
+        <h2>High Score: <span id='highScore'></span></h2>
+        <h2>Score: <span id='score'></span></h2>
+      </div>
+      <div className='board'>
+        <h3>Click on every number only once!</h3>
+      </div>
     </div>
   );
 }

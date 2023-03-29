8dbc5a461b1dd7ea869b0a4a2b373392c0d73d52
FrodeBerg <bergstedt.frode@gmail.com>
2023-03-29 18:32:44 +0200

    Add score, level, highScore, numbers state

--- src/App.js
+++ src/App.js
@@ -3,17 +3,33 @@ import { useState } from "react";
 
 
 function App() {
+  let [numbers, setNumbers] = useState([])
+  let [level, setLevel] = useState(0)
+  let [score, setScore] = useState(0)
+  let [highScore, setHighScore] = useState(0)
+
+
+
   return (
     <div>
       <div className='scores'>
         <h2>High Score: <span id='highScore'></span></h2>
         <h2>Score: <span id='score'></span></h2>
       </div>
-      <div className='board'>
+      <div>
         <h3>Click on every number only once!</h3>
+        <GameBoard /> 
       </div>
     </div>
   );
+}
+
+function GameBoard(){
+  return (
+    <div className='board'>
+
+    </div>
+  )
 }
 
 export default App;
